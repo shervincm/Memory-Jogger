aa628d9873cce2cae3e460840e5e8e889074ec8c
gregor <gregor.aubrey.uk@gmail.com>
2023-04-25 16:27:09 +0100

    added <br> between textarea, and input, and button

--- flashcard/src/components/App/App.js
+++ flashcard/src/components/App/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import React from 'react';
-import Input from '../Input';
+import "./App.css";
+import React from "react";
+import Input from "../Input";
 
-const data = []
+const data = [];
 let randomInteger;
 let titleValue;
 let factValue;
+
 
 function giveRandomInteger() {
   // this function will give a random number between 0 and the length of the array
@@ -27,7 +28,11 @@ function handleFactChange() {
 function App() {
   return (
     <div className="App">
-      <Input titleValue={titleValue} factValue={factValue} handleSubmit={handleSubmit}/>
+      <Input
+        titleValue={titleValue}
+        factValue={factValue}
+        handleSubmit={handleSubmit}
+      />
       <Output />
     </div>
   );
--- flashcard/src/components/Input/index.js
+++ flashcard/src/components/Input/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import Button from '../Button';
+import React from "react";
+import Button from "../Button";
+import { useState } from "react";
 
-function Input({ titleValue, factValue, handleSubmit}) {
+function Input({ titleValue, factValue, handleSubmit }) {
+  const [title, setTitle] = useState(titleValue);
+  const [fact, setFact] = useState(factValue);
+
   return (
     <div>
       <form>
-        <label htmlFor="Title">Title</label>
-        <label htmlFor="Fact">Fact</label>
+        <input required placeholder="Add Title" htmlFor="Title" type="text" value={titleValue} onChange={(e)=> {setTitle(e.target.value)}}></input>
+        <br></br>
+        <textarea required placeholder="Add Fact" htmlFor="Fact" type="textarea" value={factValue} onChange={(e)=> {setFact(e.target.value)}}></textarea>
+        <br></br>
+        <Button handleSubmit={handleSubmit} />
       </form>
-      <Button handleSubmit={handleSubmit}/>
     </div>
   );
 }

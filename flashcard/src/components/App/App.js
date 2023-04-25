46d059d0825628b7968c51e86ce2dce6ec97de1e
Trip25 <t.gurung1995@gmail.com>
2023-04-25 16:20:50 +0100

    added random number generator and comments

--- flashcard/src/components/App/App.js
+++ flashcard/src/components/App/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 // created state for input. Initally set as an empty string
 const [input, setInput] = useState('')
+const [quiz, setQuiz] = useState({})
 
 // function for input value
 function inputField(event){
@@ -13,10 +14,17 @@ function inputField(event){
   // create a new array with the spread operator with the value of the input
   const quizArray = [...input] 
 }
+//create random number generator
+function randomNumberGenerator(quizArray){
+  // create a variable for the random number
+  //goes through array quizArray and pick a random number
+  const randomNum = Math.floor(Math.random() * quizArray.length)
+  // return the random number
+  return randomNum
+}
 
 function quizField(){
-  const [quiz, setQuiz] = useState({})
-  setQuiz 
+  setQuiz()
 }
 
 function App() {

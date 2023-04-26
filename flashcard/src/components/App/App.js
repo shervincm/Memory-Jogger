68e47511812493938ce4e0fe4ad1e480b89887a6
gregor <gregor.aubrey.uk@gmail.com>
2023-04-26 10:07:00 +0100

    got the app to render but data is undefined at anonymous according to the console

--- flashcard/src/components/App/App.js
+++ flashcard/src/components/App/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Input from "../Input";
 import { useState } from 'react';
 
+function App() {
+
 // created state for input. Initally set as an empty string
 const [input, setInput] = useState('')
 const [quiz, setQuiz] = useState({})
@@ -11,8 +13,6 @@ const [quiz, setQuiz] = useState({})
 const [data, setData] = useState([])
 
 let randomInteger;
-let titleValue;
-let factValue;
 
 
 function giveRandomInteger() {
@@ -20,9 +20,10 @@ function giveRandomInteger() {
   randomInteger = Math.floor(Math.random() * data.length);
 }
 
-function handleSubmit() {
+function handleSubmit(title, fact) {
   // this function, when called, will add the value of Title and Fact to the end of the data array inside one object
-  setData([...data, {title: titleValue, fact: factValue, uniqueId: data.length}])
+  console.log('handleSubmit called by Input component');
+  setData([...data, {title: title, fact: fact, uniqueId: data.length}])
   // we may need to rename uniqueID to key/Key
 }
 
@@ -48,17 +49,12 @@ function quizField(){
   setQuiz()
 }
 
-function App() {
   return (
 <div className="App">
-      <Input
-        titleValue={titleValue}
-        factValue={factValue}
-        handleSubmit={handleSubmit}
-      />
-      <Form/>
+      <Input handleSubmit={handleSubmit} />
+      {/* <Form/>
       <Quiz/>
-      <Display/>
+      <Display/> */}
     </div>
   );
 }
--- flashcard/src/components/Input/index.js
+++ flashcard/src/components/Input/index.js
@@ -2,16 +2,25 @@ import React from "react";
 import Button from "../Button";
 import { useState } from "react";
 
-function Input({ titleValue, factValue, handleSubmit }) {
-  const [title, setTitle] = useState(titleValue);
-  const [fact, setFact] = useState(factValue);
+function Input({ handleSubmit }) {
+  const [title, setTitle] = useState("");
+  const [fact, setFact] = useState("");
+
+
+  function handleClick(e) {
+    e.preventDefault();
+    if (fact.trim() !== "" || title.trim() !== "") {
+    handleSubmit(title, fact);
+    console.log(`called the handleSubmit function with title of ${title} and fact of ${fact}`);
+  }
+}
 
   return (
     <div>
-      <form>
-        <input required placeholder="Add Title" htmlFor="Title" type="text" value={titleValue} onChange={(e)=> {setTitle(e.target.value)}}></input>
+      <form onSubmit={handleClick}>
+        <input required placeholder="Add Title" htmlFor="Title" type="text" value={title} onChange={(e)=> {setTitle(e.target.value)}}></input>
         <br></br>
-        <textarea required placeholder="Add Fact" htmlFor="Fact" type="textarea" value={factValue} onChange={(e)=> {setFact(e.target.value)}}></textarea>
+        <textarea required placeholder="Add Fact" htmlFor="Fact" type="textarea" value={fact} onChange={(e)=> {setFact(e.target.value)}}></textarea>
         <br></br>
         <Button handleSubmit={handleSubmit} />
       </form>

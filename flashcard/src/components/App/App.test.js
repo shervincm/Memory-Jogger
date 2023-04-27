f039a524db771d04a6fcce56c8b704c662d3a275
Trip25 <t.gurung1995@gmail.com>
2023-04-27 12:15:29 +0100

    attempted to add more tests

--- flashcard/src/components/App/App.test.js
+++ flashcard/src/components/App/App.test.js
@@ -48,8 +48,8 @@ test("when Add Flashcard is clicked, the title and fact get emptied", () => {
 });
 
 
-//test to check the array changes when the add button is clicked -Test PASS
-test("adds new flashcard object to data array when Add Flashcard button is clicked", () => {
+//test to check the array changes when the add button is clicked and also tests to see if its the same after page refresh -Test PASS
+test("adds new flashcard object to data array when Add Flashcard button is clicked and also tests to see if its the same after page refresh", () => {
   render(<App />);
   // Find the input fields for the flashcard title and fact
   const titleInput = screen.getByPlaceholderText(/Add Title/);
@@ -65,6 +65,12 @@ test("adds new flashcard object to data array when Add Flashcard button is click
   const newFlashcard = data[data.length - 1];
   expect(newFlashcard.usertitle).toEqual("New Flashcard Title");
   expect(newFlashcard.fact).toEqual("New Flashcard Fact");
+
+  //page refresh
+  window.location.reload();
+  expect(newFlashcard.usertitle).toEqual("New Flashcard Title");
+  expect(newFlashcard.fact).toEqual("New Flashcard Fact");
+
 });
 
 
@@ -82,6 +88,7 @@ test("clicking Clear Facts button deletes local storage", () => {
   // Find the "Add Flashcard" button and click it
   const addFlashcardButton = screen.getByText("Add Flashcard");
   fireEvent.click(addFlashcardButton);
+
   // Check that the local storage has been updated
   expect(localStorage.getItem("data")).not.toBe(null);
 
@@ -92,3 +99,16 @@ test("clicking Clear Facts button deletes local storage", () => {
   // Check that the local storage has been deleted
   expect(localStorage.getItem("data")).toBe(null);
 });
+
+// once test me button is clicked, the flashcard(<p></p>) is displayed - NOT WORKING YET -come back if have time
+// test("once test me button is clicked, the flashcard(<p></p>) is displayed", () => {
+//   render(<App />);
+//   const testMeButton = screen.getByRole('button', { name: /Test Me/i });
+//   fireEvent.click(testMeButton);
+  
+//   // Find all <p> elements and filter the results to select the ones we need
+//   const allPElements = screen.queryAllByRole("paragraph");
+  
+//   // Check that at least one <p> element is displayed
+//   expect(allPElements[0]).toBeVisible();
+// });
